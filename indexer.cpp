b715cedf3d5101bb187e4dbdfb8103e1bccb5236
Jesse Tremblay <jessetrem@gmail.com>
2017-09-22 21:57:11 -0400

    added basic map for testing

--- indexer.cpp
+++ indexer.cpp
@@ -1,6 +1,12 @@
 // TODO complete the assignment
 // https://moodle.concordia.ca/moodle/pluginfile.php/2916064/mod_resource/content/1/a1.pdf
+#include <iostream>
+#include <map>
+#include <string>
 
 int main() {
+  std::map<std::string, int> dictionary;
+  dictionary["test"] = 20;
+  std::cout << dictionary["test"] << std::endl;
   return 0;
 }
