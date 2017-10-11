92041c653763fed5247bdd034eea775b81372731
Christopher Dufort <c_dufor@encs.concordia.ca>
2017-10-11 18:31:06 -0400

    added "FLAGS = -std=c++11" to makefile

--- makefile.mk
+++ makefile.mk
@@ -3,12 +3,13 @@ OBJS = $(OUTDIR)\indexer.o $(OUTDIR)\document.o
 CC = g++
 DEBUG = -g
 CFLAGS = -c $(DEBUG)
+FLAGS = -std=c++11
 
 app.exe: $(OBJS)
-	$(CC) $(DEBUG) $(OBJS) -o app.exe
+	$(CC) $(DEBUG) $(FLAGS) $(OBJS) -o app.exe
 
 $(OUTDIR)\indexer.o : indexer.cpp indexer.h
-	$(CC) $(CFLAGS) indexer.cpp -o $(OUTDIR)\indexer.o
+	$(CC) $(CFLAGS) $(FLAGS) indexer.cpp -o $(OUTDIR)\indexer.o
 
 $(OUTDIR)\document.o : document.cpp document.h
-	$(CC) $(CFLAGS) document.cpp -o $(OUTDIR)\document.o
+	$(CC) $(CFLAGS) $(FLAGS) document.cpp -o $(OUTDIR)\document.o
