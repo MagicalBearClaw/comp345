2a817b1e7e1c5af35de6a6ae02ffadcec60300d2
jesse Tremblay <jessetrem@gmail.com>
2017-10-14 17:31:08 -0400

    delete test.cpp

--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "stopword.h"
-
-int main() {
-  StopWord s("sw.txt");
-  std::cout << s("this") << s("is") << s("not") << s("a") << s("test") << std::endl;
-  return 0;
-}
